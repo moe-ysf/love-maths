f213670fedbe4bbb07bd26bc56c1ae7df8990d08
Moe Yusuf <dnv9kg9k@students.codeinstitute.net>
2024-12-11 03:57:02 +0000

    added function for button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,23 @@
+
+//get the button elements and add event listeners to them 
+
+// Wait for DOM to finish loading before running the game
+
+document.addEventListener("DOMContentLoaded", function () {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function () {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        });
+    }
+});
+
 function runGame(){
     
     }
